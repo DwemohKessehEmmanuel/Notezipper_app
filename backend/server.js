fe62a07314c58e2f3d8eaf8e818bd27d27ddc028
Emmanuel <3samkus@gmail.com>
2022-11-15 14:53:50 +0000

    init commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const notes = require('./data/notes');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Our API is running')
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+app.get('/api/notes/:id', (req, res) => {
+    const note = notes.find((n) => n._id === req.params.id)
+    res.send(note)
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server is listening at port ${PORT}`)
+})
