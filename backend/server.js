ab663dacbb0bf343c20738d675d982b4d4a7346f
Emmanuel <3samkus@gmail.com>
2022-12-28 02:26:09 +0000

    db auth login create

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,32 @@ const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db')
+const userRoutes = require('./Routes/userRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
-connectDB()
+connectDB();
+app.use(express.json());
+
+
+
+
+
+// app.get('/', (req, res) => {
+//     res.send('Our API is running')
+// })
+
+// app.get('/api/notes', (req, res) => {
+//     res.json(notes)
+// })
+
+
+app.use('/api/users', userRoutes)
+app.use(notFound)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000; 
-
-app.get('/', (req, res) => {
-    res.send('Our API is running')
-})
-
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-})
-app.get('/api/notes/:id', (req, res) => {
-    const note = notes.find((n) => n._id === req.params.id)
-    res.send(note)
-})
-
 
 app.listen(PORT, () => {
     console.log(`server is listening at port ${PORT}`)
